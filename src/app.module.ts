2fd5a42588ddc83b02a0713be655c481dc423e00
Pauline Soubrie <pauline_soubrie@hotmail.fr>
2024-04-23 17:23:46 +0200

    fix user-tasks module bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { UsersModule } from './users/users.module';
-import { ProfilesModule } from './profiles/profiles.module';
-import { TasksModule } from './tasks/tasks.module';
 import { EventsModule } from './events/events.module';
 import { NotificationsModule } from './notifications/notifications.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ProfilesModule } from './profiles/profiles.module';
 import { TaskEventsModule } from './task-events/task-events.module';
-import { UserTaskEventsModule } from './user-task-events/user-task-events.module';
+import { TasksModule } from './tasks/tasks.module';
 import { UserNotificationsModule } from './user-notifications/user-notifications.module';
-import { UsertasksModule } from './usertasks/usertasks.module';
+import { UserTaskEventsModule } from './user-task-events/user-task-events.module';
 import { UserTasksModule } from './user-tasks/user-tasks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, ProfilesModule, TasksModule, EventsModule, NotificationsModule, TaskEventsModule, UserTaskEventsModule, UserNotificationsModule, UsertasksModule, UserTasksModule],
+  imports: [
+    PrismaModule,
+    UsersModule,
+    ProfilesModule,
+    TasksModule,
+    EventsModule,
+    NotificationsModule,
+    TaskEventsModule,
+    UserTaskEventsModule,
+    UserNotificationsModule,
+    UserTasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
